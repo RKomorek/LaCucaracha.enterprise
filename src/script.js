04329b224dbb0de6ccde41ac1195321a00f94e24
Komorek <rafa.kaquino@gmail.com>
2024-04-12 16:56:19 -0300

    First commit, main

---
+++ src/script.js
@@ -0,0 +1,5 @@
+document.getElementById('toggleDrawer').addEventListener('click', function() {
+    document.getElementById('drawer').style.left =
+      document.getElementById('drawer').style.left === '0px' ? '-250px' : '0px';
+  });
+  
