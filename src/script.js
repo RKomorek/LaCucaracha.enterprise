dd0b84f8a557d6d272422b5ce9b4af8061ba373e
Komorek <rafa.kaquino@gmail.com>
2024-06-05 23:40:21 -0300

    mudanca para hamberguer

--- src/script.js
+++ src/script.js
@@ -1,5 +1,35 @@
-document.getElementById('toggleDrawer').addEventListener('click', function() {
-    document.getElementById('drawer').style.left =
-      document.getElementById('drawer').style.left === '0px' ? '-250px' : '0px';
-  });
-  
+let currentIndex = 0;
+        const items = document.querySelectorAll('.carousel-item');
+        const totalItems = items.length;
+        let carouselInterval;
+
+        function updateCarousel() {
+            document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
+        }
+
+        function showNextItem() {
+            currentIndex = (currentIndex + 1) % totalItems;
+            updateCarousel();
+        }
+
+        function showPrevItem() {
+            currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+            updateCarousel();
+        }
+
+        function nextItem() {
+            clearInterval(carouselInterval);
+            showNextItem();
+            carouselInterval = setInterval(showNextItem, 5000);
+        }
+
+        function prevItem() {
+            clearInterval(carouselInterval);
+            showPrevItem();
+            carouselInterval = setInterval(showNextItem, 5000);
+        }
+
+        document.addEventListener('DOMContentLoaded', () => {
+            updateCarousel();
+            carouselInterval = setInterval(showNextItem, 5000);
+        });
