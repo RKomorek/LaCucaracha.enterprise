a5e00adb386b1ecf290bf1fa3f2d8d08563d3d87
Komorek <rafa.kaquino@gmail.com>
2024-06-06 23:46:18 -0300

    add footer

--- src/script.js
+++ src/script.js
@@ -1,35 +1,43 @@
-let currentIndex = 0;
-        const items = document.querySelectorAll('.carousel-item');
-        const totalItems = items.length;
-        let carouselInterval;
+document.addEventListener('DOMContentLoaded', () => {
+    const banners = document.querySelectorAll('.carousel-item');
 
-        function updateCarousel() {
-            document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
-        }
-
-        function showNextItem() {
-            currentIndex = (currentIndex + 1) % totalItems;
-            updateCarousel();
-        }
-
-        function showPrevItem() {
-            currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-            updateCarousel();
-        }
-
-        function nextItem() {
-            clearInterval(carouselInterval);
-            showNextItem();
-            carouselInterval = setInterval(showNextItem, 5000);
-        }
-
-        function prevItem() {
-            clearInterval(carouselInterval);
-            showPrevItem();
-            carouselInterval = setInterval(showNextItem, 5000);
-        }
-
-        document.addEventListener('DOMContentLoaded', () => {
-            updateCarousel();
-            carouselInterval = setInterval(showNextItem, 5000);
+    banners.forEach((banner) => {
+        banner.addEventListener('click', () => {
+            window.open('produtos.html', '_blank'); // Redireciona para a página de produtos em uma nova aba
         });
+    });
+
+    // Funções do carrossel existentes
+    let currentIndex = 0;
+    const totalItems = banners.length;
+    let carouselInterval;
+
+    function updateCarousel() {
+        document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    function showNextItem() {
+        currentIndex = (currentIndex + 1) % totalItems;
+        updateCarousel();
+    }
+
+    function showPrevItem() {
+        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+        updateCarousel();
+    }
+
+    function nextItem() {
+        clearInterval(carouselInterval);
+        showNextItem();
+        carouselInterval = setInterval(showNextItem, 3000); // Reinicia o intervalo
+    }
+
+    function prevItem() {
+        clearInterval(carouselInterval);
+        showPrevItem();
+        carouselInterval = setInterval(showNextItem, 3000); // Reinicia o intervalo
+    }
+
+    updateCarousel();
+    carouselInterval = setInterval(showNextItem, 3000); // Inicia o carrossel automático
+});
